44922fd917fe8c8f49de8f894d10429a0a406534
German Bertolini <german.bertolini@gmail.com>
2021-06-07 18:36:09 -0300

    AppComponent: El container pasa a ser fluid + Se comienza con DevicesComponent

---
+++ Natom.AccessMonitor.WebApp/ClientApp/src/app/devices/devices.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-devices',
+  templateUrl: './devices.component.html'
+})
+export class DevicesComponent implements OnInit {
+
+    
+
+    ngOnInit(): void {
+        
+    }
+
+}
