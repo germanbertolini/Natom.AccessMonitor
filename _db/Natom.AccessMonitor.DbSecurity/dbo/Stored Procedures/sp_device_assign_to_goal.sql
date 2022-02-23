d3eb0554b1c10060259c57b2fa7cc375e4e440e6
German Bertolini <german.bertolini@gmail.com>
2022-02-23 18:01:11 -0300

    AssignDevice: Cuando se vincula un Device a una porteria, si este ya insertó movimientos con GoalId NULL, se hace un UPDATE para setearle el GoalId NULO por el valor actual de GoalId del Device

--- _db/Natom.AccessMonitor.DbSecurity/dbo/Stored Procedures/sp_device_assign_to_goal.sql
+++ _db/Natom.AccessMonitor.DbSecurity/dbo/Stored Procedures/sp_device_assign_to_goal.sql
@@ -4,8 +4,45 @@
 AS
 BEGIN
 
+	DECLARE @InstanceId NVARCHAR(32);
+	DECLARE @DeviceId INT;
+	DECLARE @ClientId INT;
+
+	SELECT
+		@InstanceId = InstanceId,
+		@DeviceId = DeviceId
+	FROM
+		[dbo].[Device] WITH(NOLOCK)
+	WHERE
+		Id = @Id;
+
+
+	SELECT
+		@ClientId = P.ClientId
+	FROM
+		[$(DbMaster)].[dbo].[Goal] G WITH(NOLOCK)
+		INNER JOIN [$(DbMaster)].[dbo].[Place] P WITH(NOLOCK) ON P.PlaceId = G.PlaceId
+	WHERE
+		G.GoalId = @GoalId;
+
+
+	--PRIMERO ACTUALIZAMOS TODOS LOS MOVIMIENTOS SIN GOALID, QUE ES LO MAS COSTOSO
+	DECLARE @SQL NVARCHAR(4000)
+	SET @SQL = '
+		UPDATE [$(DbMaster)].[dbo].[zMovement_Client' + CAST(@ClientId AS VARCHAR) + '] WITH(READPAST)
+			SET GoalId = ' + CAST(@GoalId AS VARCHAR) + ' 
+			WHERE InstanceId = ''' + @InstanceId + ''' 
+				AND DeviceId = ' + CAST(@DeviceId AS VARCHAR) + ' 
+				AND GoalId IS NULL;
+	';
+	EXEC sp_executesql @SQL
+
+
+	--SI PASO TODO OK, AHORA ACTUALIZAMOS EL GOALID EN DEVICE
 	UPDATE [dbo].[Device]
 		SET GoalId = @GoalId
 		WHERE Id = @Id;
+
+
 
 END
